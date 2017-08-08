b1aa258856227d6090bd6ace3c83a3eb9e7eb5d5
Phayao Boonon <phayaob@gmail.com>
2017-08-08 22:12:42 +0700

    Add Authentication

--- RoomsQuickly/app.js
+++ RoomsQuickly/app.js
@@ -4,10 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var api = require('./routes/api');
-var users = require('./routes/users');
+var passport = require('passport');
+var session = require('express-session');
 var mongoose = require('mongoose');
 
 // initialize models
@@ -15,6 +13,10 @@ require('./models/model.js');
 
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost:27017/roomsquickly")
+
+var index = require('./routes/index');
+var api = require('./routes/api');
+var authenticate = require('./routes/authenticate')(passport);
 
 var app = express();
 
@@ -25,14 +27,19 @@ app.set('view engine', 'pug');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(session({
+  secret: 'super duper secret'
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+api.use(passport.session());
 
 app.use('/', index);
 app.use('/api', api);
-app.use('/users', users);
+app.use('/auth', authenticate);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -40,6 +47,10 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
+
+// initialize passport
+var initPassport = require('./passport-init');
+initPassport(passport);
 
 // error handler
 app.use(function(err, req, res, next) {
---
+++ RoomsQuickly/routes/authenticate.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+
+module.exports = function(passport) {
+    //
+    router.get('/success', function(req, res) {
+        res.send({state: 'success', user: req.user ? req.user : null});
+    });
+
+    router.get('/failure', function(req, res) {
+        res.send({state: 'failure', user: null, message: "Invalid username or password"});
+    });
+
+    router.post('/login', passport.authenticate('login', {
+        successRedirect: '/auth/success',
+        failureRedirect: '/auth/failure'
+    }));
+
+    router.post('signup', passport.authenticate('singup', {
+        successRedirect: '/auth/success',
+        failureRedirect: '/auth/failure'
+    }));
+
+    router.get('/signout', function(req, res) {
+        res.logout(),
+        res.redirect('/')
+    });
+
+    return router;
+}
